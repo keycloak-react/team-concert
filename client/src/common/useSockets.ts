bef68df3d78585b55b35ff12e31cab03dffa28df
raubin-kumar <55228116+raubin-kumar@users.noreply.github.com>
2023-03-20 14:39:41 +0530

    Random id for all supported browsers

--- client/src/common/useSockets.ts
+++ client/src/common/useSockets.ts
@@ -6,8 +6,20 @@ const SOCKET_BASE_URL = "https://team-concert-server.onrender.com";
 
 let _socket: Socket;
 
+const getRandomId = () => {
+  const array = new Array(5)
+    .fill(0)
+    .map((i) => Math.floor(Math.random() * 10000000000));
+  const idArr: string[] = [];
+  array.forEach((item) => {
+    idArr.push(item.toString(36));
+  });
+
+  return idArr.join("-");
+};
+
 function createSocket(url: string, room: string, name: string) {
-  const id = crypto.randomUUID();
+  const id = getRandomId();
   if (_socket) return _socket;
   _socket = io(url, {
     transports: ["websocket", "polling"],
